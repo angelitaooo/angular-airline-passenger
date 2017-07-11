3a85dcbf5e1d026c55b58c5d549a28579f062a73
Angela ordonez <angela.ordonez@yuxiglobal.com>
2017-07-11 10:28:55 -0500

    adding ngOnChanges lifecycle hook

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -8,6 +8,9 @@ import { Passenger } from '../../models/passenger.interface';
         <div>
             <passenger-count [items]="passengers">                
             </passenger-count>
+            <div *ngFor="let passenger of passengers">
+                {{passenger.fullname}}
+            </div>
             <passenger-detail
                  *ngFor="let passenger of passengers"
                  [detail]="passenger"
